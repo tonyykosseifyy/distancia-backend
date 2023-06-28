081c072fef335a01877487e6152cb64e18883d09
Tony Kosseify <tonykosseify123@gmail.com>
2023-06-28 18:56:16 +0300

    allow cors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,17 @@ import { AtGuard } from './common/guards';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(
+    {
+      "origin": "http://localhost:3000/",
+      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    }
+  );
   app.useGlobalPipes(new ValidationPipe());
   // app.useGlobalGuards(new AtGuard());
+  
   await app.listen(3333);
 }
 bootstrap();
+
+
